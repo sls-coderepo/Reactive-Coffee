55c8a9443540c7d8b179df0b0ddd5273134764e7
unknown <[shrestha_sl@hotmail.com]>
2019-10-07 15:04:28 -0500

    Displayed coffee card in coffee list

---
+++ src/components/coffee/CoffeeList.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import CoffeeCard from './CoffeeCard';
+import APIManager from '../../modules/APIManager';
+import './coffee.css'
+
+class CoffeeList extends Component {
+    state = {
+        allCoffee: []
+    }
+
+    componentDidMount(){
+        APIManager.getAll("coffees").then((allCoffee) => {
+            this.setState({
+                allCoffee: allCoffee
+            })
+        })
+    }
+
+    render(){
+        console.log("pls render coffee")
+        return (
+            <>
+            <h1>COFFEE LIST</h1>
+            {
+                this.state.allCoffee.map(coffee =>
+                    <CoffeeCard key={coffee.id} 
+                                coffee={coffee}
+                                />
+                )
+            }
+            </>
+        )
+    }
+}
+
+export default CoffeeList;
